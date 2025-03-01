65e69aef4a7ba53992b6f1867dfb92e36188fd83
zenzer0s <praveenzalaki200@gmail.com>
2025-03-01 14:59:28 +0530

    ✨ Remove unused comments and documentation from scheduler.js

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -1,12 +1,3 @@
-// utils/scheduler.js
-/**
- * This file is for scheduling recurring tasks
- * Currently empty, but can be used for:
- * - Weekly summary reports for users
- * - Cleanup of expired sessions
- * - Periodic notifications about GitHub activity
- */
-
 const cron = require('node-cron');
 
 function scheduleSessionCleanup(sessionManager) {
